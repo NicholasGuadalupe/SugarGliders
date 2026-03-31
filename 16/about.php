9b12bfd87b456014352603715827f72d870dce32
Carter Hickerson <chickerson@Carters-MacBook-Air-54.local>
2026-03-31 12:52:13 -0600

    instructions

---
+++ 16/about.php
@@ -0,0 +1,8 @@
+<?php
+/* 
+3.2 About Page (about.php)
+One section for each group member.
+Each section must include a name, a short bio, and a photo or graphic avatar.
+*/
+
+?>
---
+++ 16/index.php
@@ -0,0 +1,10 @@
+<?php
+
+/* 
+3.1 Homepage (index.php)
+Modern-looking design with your company name and logo.
+A welcome message explaining what the site is about.
+Navigation links to all other pages on the site.
+*/
+
+?>
