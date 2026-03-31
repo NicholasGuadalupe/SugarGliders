262dabea88922960dc8ab3858d2fb18b9b04e027
Carter Hickerson <chickerson@Carters-MacBook-Air-54.local>
2026-03-31 12:54:01 -0600

    more instructions

---
+++ 16/print.php
@@ -0,0 +1,14 @@
+<?php
+/*
+This page must:
+
+Show the same two tables with the same dimensions and selected colors as the main page.
+Display the selected color names as plain text in the cells where the dropdowns were.
+Render in greyscale.
+Include a greyscale version of your company logo and name as a header.
+Be designed to easily print on one 8.5 x 11-inch page in portrait mode.
+Hint: Think about how your color-coordinated page can pass the selected values to the print page. 
+You have learned ways to pass data between pages in this course.
+*/
+
+?>
