3d2b72b5022da83f28ad0d025499a1e26316138f
Carter Hickerson <chickerson@Carters-MacBook-Air-140.local>
2026-04-27 13:59:52 -0600

    added instructions

--- Group16/db.php
+++ Group16/db.php
@@ -1,8 +1,64 @@
 <?php
+/* 
+
+Run this to log into mysql
+Then run the command below, replacing cs-username with your own CS username:
+
+  mysql -u NETID -D NETID -h helmi -p
+
+-- =========================================
+-- INITIALIZE COLORS TABLE
+-- Run this if the table does NOT exist
+-- =========================================
+
+CREATE TABLE colors (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE,
+    hex_value VARCHAR(7) NOT NULL UNIQUE
+);
+
+INSERT INTO colors (name, hex_value) VALUES
+('Red', '#FF0000'),
+('Orange', '#FFA500'),
+('Yellow', '#FFFF00'),
+('Green', '#008000'),
+('Blue', '#0000FF'),
+('Purple', '#800080'),
+('Grey', '#808080'),
+('Brown', '#A52A2A'),
+('Black', '#000000'),
+('Teal', '#008080');
+
+-- =========================================
+-- RESET COLORS TABLE
+-- WARNING: This deletes ALL data
+-- =========================================
+
+DROP TABLE IF EXISTS colors;
+
+CREATE TABLE colors (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE,
+    hex_value VARCHAR(7) NOT NULL UNIQUE
+);
+
+INSERT INTO colors (name, hex_value) VALUES
+('Red', '#FF0000'),
+('Orange', '#FFA500'),
+('Yellow', '#FFFF00'),
+('Green', '#008000'),
+('Blue', '#0000FF'),
+('Purple', '#800080'),
+('Grey', '#808080'),
+('Brown', '#A52A2A'),
+('Black', '#000000'),
+('Teal', '#008080');
+*/
+
 $host = "helmi";
-$user = "";
-$password = "";
-$database = "";
+$user = "carter64";
+$password = "iminhillbillyhell";
+$database = "carter64";
 
 $conn = new mysqli($host, $user, $password, $database);
 
